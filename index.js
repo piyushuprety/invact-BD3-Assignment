29b3b53e6fb80a33e6d464e22b4142b3e7a07515
piyushuprety <git@stackblitz.com>
2024-12-28 12:43:33 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors')
+
+const app = express();
+app.use(cors())
+const port = 3000;
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
